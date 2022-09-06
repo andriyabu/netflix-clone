c98b06458b6cb999d7f72a94e50115534c6e2d35
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-09-07 05:06:29 +0700

    created Header.tsx component and added logo image

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <header>
+        <div>
+            <img
+                src="https://rb.gy/ulxxee"
+                width={100}
+                height={100}
+                className="cursor-pointer object-contain"
+                alt="logo"
+            />
+        </div>
+
+        <div>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
